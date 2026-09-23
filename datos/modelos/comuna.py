05aedf90c6e30d008c91ccfe32abc3890176ba44
Allison Rojas <allisonrojas765@gmail.com>
2026-09-23 15:08:04 -0300

    modelos final

--- datos/modelos/comuna.py
+++
@@ -1,5 +0,0 @@
-class comuna:
-    def __init__(self,id_comuna,comuna,habilitado):
-        self.__id_comuna = id_comuna
-        self.__comuna = comuna 
-        self.__habilitado = habilitado
--- datos/modelos/pais.py
+++
@@ -1,5 +0,0 @@
-class pais:
-    def __init__(self,id_pais,pais,habilitado):
-        self.__id_pais = id_pais
-        self.__pais = pais
-        self.__habilitado = habilitado
